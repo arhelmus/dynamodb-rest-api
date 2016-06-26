e8effc9f80e0ee49e7f74d9cf05bc6f2feee6f1b
Arthur Kushka <arthurk@wix.com>
2016-06-26 19:07:57 +0300

    Changed format of date for controllers.

--- app/utils/ExtraJsonFormats.scala
+++ app/utils/ExtraJsonFormats.scala
@@ -1,6 +1,7 @@
 package utils
 
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 import com.wix.accord.Violation
 import models.Fuel
@@ -9,19 +10,23 @@ import play.api.libs.json.{JsSuccess, _}
 trait ExtraJsonFormats {
 
   implicit val dateWrites = new Writes[LocalDate] {
-    override def writes(c: LocalDate) = JsNumber(c.toEpochDay)
+    override def writes(c: LocalDate) =
+      JsString(c.format(DateTimeFormatter.BASIC_ISO_DATE))
   }
 
   implicit val dateReads = new Reads[LocalDate] {
-    override def reads(json: JsValue): JsResult[LocalDate] = JsSuccess(LocalDate.ofEpochDay(json.as[Long]))
+    override def reads(json: JsValue): JsResult[LocalDate] =
+      JsSuccess(LocalDate.from(DateTimeFormatter.BASIC_ISO_DATE.parse(json.as[String])))
   }
 
   implicit val fuelWrites = new Writes[Fuel] {
-    override def writes(c: Fuel) = JsString(c.toString)
+    override def writes(c: Fuel) =
+      JsString(c.toString)
   }
 
   implicit val fuelReads = new Reads[Fuel] {
-    override def reads(json: JsValue): JsResult[Fuel] = JsSuccess(Fuel.apply(json.as[String]))
+    override def reads(json: JsValue): JsResult[Fuel] =
+      JsSuccess(Fuel.apply(json.as[String]))
   }
 
   implicit val violationWrites = new Writes[Violation] {
